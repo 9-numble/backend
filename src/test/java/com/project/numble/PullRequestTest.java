4d09a63b34b873bdccbd8f95424407c3af71c2c7
apptie <appti@naver.com>
2022-10-30 16:27:49 +0900

    test: PR 테스트

---
+++ src/test/java/com/project/numble/PullRequestTest.java
@@ -0,0 +1,12 @@
+package com.project.numble;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PullRequestTest {
+
+    @Test
+    void test() {
+        Assertions.assertTrue(false);
+    }
+}
